e82df4759068e3947f5f3358c47ec71193537b70
李正 <lizheng11@meituan.com>
2018-06-03 18:50:20 +0800

    main

--- main.js
+++ main.js
@@ -6,12 +6,20 @@ const parse = require('./lib/parse');
 const hosts = require('./lib/hosts');
 const Nginx = require('./lib/nginx');
 
-let nginx = new Nginx({
-    path: '/Users/lizhengnacl/liz/learn/js_/nginx/nginx.conf'
-});
+const config = {
+    nginxConf: {
+        path: '/Users/lizhengnacl/liz/learn/js_/nginx/nginx.conf',
+        // certPath: '',
+        // forceReload: false,
+    },
+    hostConfig: {
+        path: '../hosts.md'
+    }
+};
 
+let nginx = new Nginx(config.nginxConf);
 
-let conf = parse('../hosts.md');
+let conf = parse(config.hostConfig);
 
 conf.forEach((rule) => {
     hosts.set(rule.target, rule.source);
@@ -24,4 +32,17 @@ hosts.effect().then((lines) => {
 
 nginx.effect().then((conf) => {
     // nginx.clear();
+});
+
+process.stdin.resume();
+
+process.on('SIGINT', function () {
+    console.log('Got SIGINT.  Press Control-D to exit.');
+});
+
+process.on('exit', async () => {
+    // 这里必须是一个同步操作
+    hosts.clear();
+    // TODO 失效
+    nginx.clear();
 });
